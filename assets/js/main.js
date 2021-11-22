7cece6e26759028ee851be7cfbdfce132bc2c217
Joel Jimenez <yoelniz.1326@gmail.com>
2021-11-22 01:00:51 -0600

    estructura escritorio lista primera fase

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,4 +60,34 @@ $(document).ready(function(){
                 }
             }
         })
+
+        $('#catalogoInd').owlCarousel({
+            loop:true,
+            autoplay: true,
+            margin:10,
+            nav:true,
+            dots: true,
+            autoplayHoverPause: true,
+            autoplaySpeed: 1500,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+
+                900:{
+                    items:1
+                },
+
+                1200:{
+                    items:1
+                },
+
+                1500:{
+                    items:1
+                }
+            }
+        })
 });
