af3247228b0262cdad5970bcdaff8468d83f468b
Joel Jimenez <yoelniz.1326@gmail.com>
2021-11-25 02:04:43 -0600

    responsive del footer,index,mayoristas, medio catalogo genral

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
                     items:1
                 },
                 600:{
-                    items:1
+                    items:2
                 },
 
                 900:{
